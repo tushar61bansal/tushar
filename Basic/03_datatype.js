3eb4371620aa151146962a5ebbdcb8f76a449aa8
Tushar Bansal <tusharbansal5941@gmail.com>
2026-05-20 19:48:20 +0530

    Conversion of datatypes

---
+++ Basic/03_datatype.js
@@ -0,0 +1,19 @@
+let score = "33" ;
+let score1 = "33abc"
+//null => 0
+// boolean => 0/1
+
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score);
+let valueInNumber1 = Number(score1);
+
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+// "33" => 33 
+// "33abc" => NaN
+
+
